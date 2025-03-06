4a66fbbd43fef4c96cd4e3d4e9e9ea4011ae83da
alsdk1676 <alsdk6761@gmail.com>
2025-03-06 17:51:09 +0900

    jsp day02-2

--- mvc/src/main/java/com/app/product/ProductFrontController.java
+++ mvc/src/main/java/com/app/product/ProductFrontController.java
@@ -7,15 +7,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.app.Result;
+import com.app.product.controller.ProductListController;
+import com.app.product.controller.ProductWriteController;
+import com.app.product.controller.ProductWriteOkController;
+
 // ProductFC만 servlet
 public class ProductFrontController extends HttpServlet {
 	
 //	http://localhost:9000/mvc/insert.product => insert만 필요 !
-//	mvc = ContextPath 필요없음
+//	mvc(ContextPath) 필요 없음
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		req.setCharacterEncoding("UTF-8");
-//		resp.setContentType("text/html; charset=utf-8;");
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=utf-8;");
 		
 //		["insert", "product"]
 		String target = req.getRequestURI().replace(req.getContextPath() + "/", "").split("\\.")[0];
@@ -23,15 +28,30 @@ public class ProductFrontController extends HttpServlet {
 		
 		
 		if(target.equals("write")) { // forward : 작성 페이지로 이동
+//			result = new Result();
+//			result.setPath("write.jsp");
+			result = new ProductWriteController().execute(req, resp);
 		}else if(target.equals("write-ok")) {
+			result = new ProductWriteOkController().execute(req,resp);
 		}else if(target.equals("list")) {
+			result = new ProductListController().execute(req, resp);
 		}else if(target.equals("read")) {
+			
 		}else if(target.equals("update")) {
 		}else if(target.equals("update-ok")) {
 		}else if(target.equals("delete-ok")) {
 		}else {
 //			전부 404
 			
+		}
+		
+//		F.C에서 보내기
+		if(result != null) {
+			if(result.isRedirect()) {
+				resp.sendRedirect(result.getPath());
+			} else {
+				req.getRequestDispatcher(result.getPath()).forward(req, resp);			
+			}
 		}
 	}
 	
